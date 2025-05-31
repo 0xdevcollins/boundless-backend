3c722afb4006b553137f0b8347bdfe5d55ea5102
clintjeff2 <clintjeff2@gmail.com>
2025-05-31 18:34:25 +0100

    Added /admin route and ensured that all swagger documentation works properly

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ import { protect } from "./middleware/auth";
 import analyticsRoutes from "./routes/analytics.route";
 import reportsRoutes from "./routes/report.route";
 import projectRoutes from "./routes/project.route";
+import adminRoutes from "./routes/admin.routes";
 dotenv.config();
 
 connectDB();
@@ -46,7 +47,7 @@ app.use("/api/analytics", authMiddleware, analyticsRoutes);
 app.use("/api/reports", authMiddleware, reportsRoutes);
 app.use("/api/comments", authMiddleware, commentRoutes);
 app.use("/api/projects", authMiddleware, projectRoutes);
-
+app.use("/api/admin", adminRoutes);
 // Setup Swagger
 setupSwagger(app);
 
