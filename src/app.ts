f19270b46185923fed41c13cfa2b2fe2b597f327
Collins Ikechukwu <collinschristroa@gmail.com>
2025-06-28 18:22:18 +0100

    refactor: comment out unused route imports and middleware for analytics, reports, projects, and admin functionalities

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,6 @@ import notificationRoutes from "./routes/notification.route";
 import commentRoutes from "./routes/comment.route";
 import { config } from "./config";
 import { setupSwagger } from "./config/swagger";
-import analyticsRoutes from "./routes/analytics.route";
-import reportsRoutes from "./routes/report.route";
-import projectRoutes from "./routes/project.route";
-
-import adminFundingRoutes from "./routes/admin.funding.route";
-
-import adminRoutes from "./routes/admin.route";
 import blogRoutes from "./routes/blog.route";
 
 dotenv.config();
@@ -50,14 +43,14 @@ app.use("/api/auth", authRoutes);
 // Protected routes
 app.use("/api/users", router);
 app.use("/api/notifications", authMiddleware, notificationRoutes);
-app.use("/api/analytics", authMiddleware, analyticsRoutes);
-app.use("/api/reports", authMiddleware, reportsRoutes);
+// app.use("/api/analytics", authMiddleware, analyticsRoutes);
+// app.use("/api/reports", authMiddleware, reportsRoutes);
 app.use("/api/comments", authMiddleware, commentRoutes);
-app.use("/api/projects", authMiddleware, projectRoutes);
+// app.use("/api/projects", authMiddleware, projectRoutes);
 
-app.use("/api/admin/funding", adminFundingRoutes);
+// app.use("/api/admin/funding", adminFundingRoutes);
 
-app.use("/api/admin", authMiddleware, adminRoutes);
+// app.use("/api/admin", authMiddleware, adminRoutes);
 
 app.use("/api/admin/blogs", blogRoutes);
 
